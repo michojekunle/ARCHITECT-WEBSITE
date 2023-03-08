e22cffb7a18c33d5119c6a2d5881339320f16eca
AMD <michojekunle1@gmail.com>
2023-03-08 21:26:53 +0100

    Added spinner

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+window.onload = () => {
+    setTimeout(() => {
+        document.querySelector('body').classList.add("display");
+    }, 4000)
+}
 document.querySelector('.hamburger-menu').addEventListener('click', ()=> {
     document.querySelector('.container').classList.toggle('change');
 })
@@ -13,9 +18,3 @@ document.querySelectorAll('.field').forEach(el => {
     })
 })
 
-// document.querySelector('scrol-btn').addEventListener('click', () => {
-//     document.querySelector('html').style.scrollBehavior = 'smooth';
-//     setTimeout(()=> {
-//         document.querySelector('html').style.scrollBehavior = 'unset';
-//     }, 1000)
-// })
