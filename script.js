43334dfbac86f76b8937d21f7f3017ee3d50288f
AMD <michojekunle1@gmail.com>
2023-03-14 09:46:11 +0100

    made changes

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.onload = () => {
         document.querySelector('body').classList.add("display");
     }, 4000)
 }
+
 document.querySelector('.hamburger-menu').addEventListener('click', ()=> {
     document.querySelector('.container').classList.toggle('change');
 })
